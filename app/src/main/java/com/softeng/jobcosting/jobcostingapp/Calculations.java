2412fab5db2be822ea4f667d2a488d9a94640b50
rpdids <rpdids@gmail.com>
2016-02-22 14:49:54 -0600

    added Calculations to branch added a method for use in MainActivity (currently commented out)

---
+++ app/src/main/java/com/softeng/jobcosting/jobcostingapp/Calculations.java
@@ -0,0 +1,57 @@
+package com.softeng.jobcosting.jobcostingapp;
+
+public class Calculations {
+    private Database db;
+
+    public Calculations() {
+        db = GlobalDatabase.getDB();
+    }
+
+    public String newOrder() {
+        db.setTable("Orders");
+        db.insert("Date", "Date()");
+        return db.query();
+    }
+
+    public String newItem(String orderID, String store, String description, String type, String price) {
+        String result = null;
+
+        db.setTable("Costs");
+
+        //if all inserts were successful, returns the result of the query method
+        if(db.update("Store", store, "OrderID", orderID) &&
+                db.update("Description", description, "OrderID", orderID) &&
+                db.update("Type", type, "OrderID", orderID) &&
+                db.update("Price", price, "OrderID", orderID)) {
+            result = db.query();
+        }
+
+        return result;
+    }
+
+    public void editItem(String field, String newValue, int costID) {
+        db.setTable("Costs");
+        boolean success = db.update(field, newValue, "CostID", Integer.toString(costID));
+    }
+
+    public String getItems(int orderID) {
+        db.setTable("Costs");
+        boolean success = db.where("OrderID", Integer.toString(orderID));
+        return db.query();
+    }
+
+//    public int[] getOrderIDs()  {
+//        db
+//    }
+    /*public float getProfit() {
+
+    }
+
+    public float getMargin() {
+
+    }
+
+    public float getTotal() {
+
+    }*/
+}
