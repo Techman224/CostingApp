ccce4a37ad81e38c6a947944cab5b741980c71bd
AlanBridgeman <abask@live.ca>
2016-03-03 16:49:36 -0600

    new file:   app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/Actual/ActualDatabase.java 	modified:   app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/Database.java 	modified:   app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/GlobalDatabase.java 	new file:   app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/stub/StubDB.java 	new file:   app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/stub/StubDatabase.java

--- app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/GlobalDatabase.java
+++ app/src/main/java/com/softeng/jobcosting/jobcostingapp/Database/GlobalDatabase.java
@@ -3,19 +3,26 @@ package com.softeng.jobcosting.jobcostingapp.Database;
 import android.app.Application;
 
 import com.softeng.jobcosting.jobcostingapp.Database.Database;
+import com.softeng.jobcosting.jobcostingapp.Database.stub.StubDatabase;
 
 /**
  * Created by Courtney on 2016-02-21.
+ * Modified by Alan on 2016-03-02.
  */
 public class GlobalDatabase extends Application {
     private static Database db;
 
     public void onCreate() {
-        super.onCreate();
-        db = new Database();
+        ///super.onCreate();
+        db = new StubDatabase();
     }
 
     public static Database getDB() {
         return db;
     }
+	
+	public static void main(String args[]) {
+		System.out.println("Is it getting to this point?");
+		GlobalDatabase.getDB();
+	}
 }
