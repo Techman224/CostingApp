1121b744b3027c357aeb6862b64f4c8f692cfbbe
Jasmin Bissonnette <bissonnj@myumanitoba.ca>
2016-03-09 09:42:34 -0600

    created jUnit test for calculations.java

---
+++ app/src/test/java/com/softeng/jobcosting/jobcostingapp/CalculationsUnitTest.java
@@ -0,0 +1,12 @@
+package com.softeng.jobcosting.jobcostingapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculationsUnitTest {
+    @Test
+    public void newOrder_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
