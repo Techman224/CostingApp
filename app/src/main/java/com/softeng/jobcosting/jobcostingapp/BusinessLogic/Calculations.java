e31c7b96efd76216ac2ba0696e43647bd8b933bd
Techman224 <bernshi3@myumanitoba.ca>
2016-03-10 20:27:55 -0600

    Added the GlobalDatabase constructor to CalculationsTest

--- app/src/main/java/com/softeng/jobcosting/jobcostingapp/BusinessLogic/Calculations.java
+++ app/src/main/java/com/softeng/jobcosting/jobcostingapp/BusinessLogic/Calculations.java
@@ -150,4 +150,6 @@ public class Calculations {
 
         return boardTotal;
     }
+
+    public Database getDB() {return db;}
 }
--- app/src/test/java/com/softeng/jobcosting/jobcostingapp/CalculationsTest.java
+++ app/src/test/java/com/softeng/jobcosting/jobcostingapp/CalculationsTest.java
@@ -1,6 +1,8 @@
 package com.softeng.jobcosting.jobcostingapp;
 
 import com.softeng.jobcosting.jobcostingapp.BusinessLogic.Calculations;
+import com.softeng.jobcosting.jobcostingapp.Database.GlobalDatabase;
+import com.softeng.jobcosting.jobcostingapp.Database.stub.StubDatabase;
 
 import java.util.*;
 import org.junit.Test;
@@ -8,10 +10,13 @@ import static org.junit.Assert.*;
 
 public class CalculationsTest
 {
+
     @Test
     public void newOrder_isDateCorrect()
     {
+        GlobalDatabase gdb = new GlobalDatabase();
         Calculations newCalc = new Calculations();
+        assertTrue(newCalc.getDB() instanceof StubDatabase);
         System.out.println((new Date()).getDate() + "/" + (new Date()).getMonth() + "/" + (new Date()).getYear());
         Date newDate = new Date();
         assertEquals(0, newCalc.newOrder().compareTo(newDate.getDate() + "/" + newDate.getMonth() + "/" + newDate.getYear()));
