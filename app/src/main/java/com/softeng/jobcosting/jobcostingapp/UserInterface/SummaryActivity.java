189e897464f95e3b7baf0a09bd13a6554b164613
rpdids <rpdids@gmail.com>
2016-03-11 23:51:25 -0600

    Removed menu from ActionToolbar in SummaryActivity

--- app/src/main/java/com/softeng/jobcosting/jobcostingapp/UserInterface/SummaryActivity.java
+++ app/src/main/java/com/softeng/jobcosting/jobcostingapp/UserInterface/SummaryActivity.java
@@ -107,13 +107,6 @@ public class SummaryActivity extends AppCompatActivity {
         return parsedAll;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
 
 
 }
