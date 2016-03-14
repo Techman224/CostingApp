8d01d8a681b86c909a41cf2eeba652f02d8d557c
rpdids <rpdids@gmail.com>
2016-03-13 23:51:33 -0500

    Finished/fixed Calculations.getOrderIDs() Now fully implements database (commented out before) and added a check in case the Orders table of the database is empty

--- app/src/main/java/com/softeng/jobcosting/jobcostingapp/BusinessLogic/Calculations.java
+++ app/src/main/java/com/softeng/jobcosting/jobcostingapp/BusinessLogic/Calculations.java
@@ -83,24 +83,29 @@ public class Calculations {
     public int[] getOrderIDs()  {
 
         db.setTable("Orders");
-//        db.select();
-        String []orderTable = db.query().split("\n");
-        String unfmtdNums = "";
+        db.select();
+        String strTable = db.query();
+        String []orderTable = null;
+        int []orderNums = null;
 
-        for(int i = 0; i < orderTable.length; i++)  {
-            unfmtdNums += orderTable[i].split(",")[0];
-            if(i != orderTable.length-1)   {
-                unfmtdNums += ",";
+        if(!strTable.equals("")) {
+            strTable.split("\n");
+            String unfmtdNums = "";
+
+            for (int i = 0; i < orderTable.length; i++) {
+                unfmtdNums += orderTable[i].split(",")[0];
+                if (i != orderTable.length - 1) {
+                    unfmtdNums += ",";
+                }
+            }
+
+            String[] unfmtdArray = unfmtdNums.split(",");
+            orderNums = new int[unfmtdArray.length];
+
+            for (int i = 0; i < orderNums.length; i++) {
+                orderNums[i] = Integer.parseInt(unfmtdArray[i]);
             }
         }
-
-        String[] unfmtdArray = unfmtdNums.split(",");
-        int[] orderNums = new int[unfmtdArray.length];
-
-        for(int i = 0; i < orderNums.length; i++)   {
-            orderNums[i] = Integer.parseInt(unfmtdArray[i]);
-        }
-
 
 
         return orderNums;
