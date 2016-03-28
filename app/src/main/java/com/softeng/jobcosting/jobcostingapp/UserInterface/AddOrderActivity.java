6d57c6b3514049f80456bb8e58da793fa86a49e1
MayRussell <c.may.russell@gmail.com>
2016-03-28 02:19:26 -0500

    Added error message popups for invalid input or missing required input fields.

--- app/src/main/java/com/softeng/jobcosting/jobcostingapp/UserInterface/AddOrderActivity.java
+++ app/src/main/java/com/softeng/jobcosting/jobcostingapp/UserInterface/AddOrderActivity.java
@@ -7,25 +7,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.MotionEvent;
+import android.view.View.OnTouchListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import com.softeng.jobcosting.jobcostingapp.BusinessLogic.Calculations;
 import com.softeng.jobcosting.jobcostingapp.R;
 
 import java.util.ArrayList;
 
-public class AddOrderActivity extends AppCompatActivity {
+public class AddOrderActivity extends AppCompatActivity implements OnTouchListener {
     private ArrayList<View> items;
-    private Calculations calc;
     private int orderID;
+    private boolean validStore;
+    private boolean validAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -39,28 +45,19 @@ public class AddOrderActivity extends AppCompatActivity {
         items.add(mView);
         mainLayout.addView(mView);
 
-        //create a new order
-        calc = new Calculations();
-
-        //returns the orderID and date of the new order, separated by a comma
-        String newOrder = calc.newOrder();
-
-        final int ORDER_ID = 0;
-        final int DATE = 1;
-
-        String[] values = newOrder.split(",");
-
         //display the orderID
-        orderID = Integer.parseInt(values[ORDER_ID]);
-        TextView orderNum = (TextView)findViewById(R.id.orderNumber);
-        String orderNumView = orderNum.getText().toString();
-        orderNumView += " " + Integer.toString(orderID);
-        orderNum.setText(orderNumView);
+        //orderID = Integer.parseInt(values[ORDER_ID]);
+        //TextView orderNum = (TextView)findViewById(R.id.orderNumber);
+        //String orderNumView = orderNum.getText().toString();
+        //orderNumView += " " + Integer.toString(orderID);
+        //orderNum.setText(orderNumView);
 
-        //display the date
-        TextView date = (TextView)findViewById(R.id.date);
+        //display the current date
+        Date currDate = new Date();
+        String formattedDate = new SimpleDateFormat("dd/MM/yyyy").format(currDate);
+        TextView date = (TextView) findViewById(R.id.date);
         String dateView = date.getText().toString();
-        dateView += " " + values[DATE];
+        dateView += " " + formattedDate;
         date.setText(dateView);
     }
 
@@ -76,9 +73,10 @@ public class AddOrderActivity extends AppCompatActivity {
 
     //Called when DONE button is clicked
     public void done(View view) {
-        boolean valid = true;
+        validStore = true;
+        validAmount = true;
 
-        for(int i = 0; i < items.size() ; i++) {
+        for(int i = 0; i < items.size(); i++) {
             EditText storeInput = (EditText) items.get(i).findViewById(R.id.storeEditText);
             String store = storeInput.getText().toString();
 
@@ -91,19 +89,67 @@ public class AddOrderActivity extends AppCompatActivity {
             EditText amountInput = (EditText) items.get(i).findViewById(R.id.amtEditText);
             String stringAmount = amountInput.getText().toString();
 
-            if(!store.equals("") && !type.equals("") && !stringAmount.equals("")) {
-                float amount = Float.parseFloat(stringAmount);
-                calc.newItem(orderID, store, description, type, amount);
-            } else {
-                valid = false;
+            if (store.equals("")) {
+                validStore = false;
+            }
+
+            if (stringAmount.equals("") || !stringAmount.matches("\\s*[-+]?\\d*(\\.\\d{1,2})?\\s*")) {
+                validAmount = false;
             }
         }
 
-        if(!valid) {
-            calc.deleteOrder(orderID);
-        }
+        if(!validStore) {
+            TextView errorPopup = (TextView) findViewById(R.id.errorMessage);
+            String errorString = errorPopup.getText().toString();
+            errorString += "Please enter a store for each item.";
+            errorPopup.setText(errorString);
+            View popup = findViewById(R.id.popup);
+            popup.setVisibility(View.VISIBLE);
+            popup.setOnTouchListener(this);
+        } else if(!validAmount) {
+            TextView errorPopup = (TextView) findViewById(R.id.errorMessage);
+            String errorString = errorPopup.getText().toString();
+            errorString += "Please enter a valid amount for each item.";
+            errorPopup.setText(errorString);
+            View popup = findViewById(R.id.popup);
+            popup.setVisibility(View.VISIBLE);
+            popup.setOnTouchListener(this);
+        } else {
+            //create a new order
+            Calculations calc = new Calculations();
+            String newOrder = calc.newOrder();
 
-        Intent returnIntent = new Intent(this, MainActivity.class);
-        startActivity(returnIntent);
+            //get orderID FOR NOW will eventually be putting it in myself.
+            String[] values = newOrder.split(",");
+            orderID = Integer.parseInt(values[0]);
+
+            for(int i = 0; i < items.size(); i++) {
+                EditText storeInput = (EditText) items.get(i).findViewById(R.id.storeEditText);
+                String store = storeInput.getText().toString();
+
+                Spinner types = (Spinner) items.get(i).findViewById(R.id.typeSpinner);
+                String type = (types.getSelectedItem()).toString();
+
+                EditText descriptionInput = (EditText) items.get(i).findViewById(R.id.descEditText);
+                String description = descriptionInput.getText().toString();
+
+                EditText amountInput = (EditText) items.get(i).findViewById(R.id.amtEditText);
+                String stringAmount = amountInput.getText().toString();
+                float amount = Float.parseFloat(stringAmount);
+
+                calc.newItem(orderID, store, description, type, amount);
+            }
+
+            Intent returnIntent = new Intent(this, MainActivity.class);
+            startActivity(returnIntent);
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        v.setVisibility(View.GONE);
+        TextView errorPopup = (TextView) v.findViewById(R.id.errorMessage);
+        errorPopup.setText("ERROR:\n");
+        return true;
     }
 }
